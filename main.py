24667291096de23bd083540c4281a0cbd3c4afa0
7wick <saatwick.chandra2201@gmail.com>
2022-07-24 18:22:22 +0530

    code commit

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from typing import List
+class Slot:
+    def __init__(self, a, b):
+        self.time = a
+        self.index = b
 
-input_file = open('1.in')
+
+input_file = open("input_files/10.in")
 rows = input_file.read().strip().split('\n')
 total_lifeguards = int(rows[0])
+slotList = [0] * 2 * total_lifeguards
+actual_max_time = 0
+unique_time_list = [0] * total_lifeguards
+last_time = 0
+unique_time_set = set()
 
-lifeguards_shift_list: List[List[int]] = list()  # declaring list of list in python3
 for lifeguard in range(total_lifeguards):
-	shift_slots = rows[lifeguard + 1].split(' ')
-	shift_slots_list = list(map(int, shift_slots))
-	lifeguards_shift_list.append(shift_slots_list)
+    shift_time = rows[lifeguard + 1].split(' ')
+    slotList[2 * lifeguard] = Slot(int(shift_time[0]), lifeguard)
+    slotList[2 * lifeguard + 1] = Slot(int(shift_time[1]), lifeguard)
 
-print(lifeguards_shift_list)
+slotList.sort(key=lambda state: state.time)
+
+for slot in slotList:
+    if len(unique_time_set) == 1:
+        for loneitem in unique_time_set:
+            unique_time_list[loneitem] += slot.time - last_time
+    if len(unique_time_set) > 0:
+        actual_max_time += slot.time - last_time
+    if slot.index in unique_time_set:
+        unique_time_set.remove(slot.index)
+    else:
+        unique_time_set.add(slot.index)
+    last_time = slot.time
+
+maximum_common_time = 0
+for lifeguard_unique_time in unique_time_list:
+    maximum_common_time = max(maximum_common_time, actual_max_time - lifeguard_unique_time)
+print(maximum_common_time)
